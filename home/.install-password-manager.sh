1c704caa0ffa584ab146a1d14b986fc69a93a4c9
Timothy McFadden <mtik00@users.noreply.github.com>
2024-08-21 09:10:52 -0600

    fix: multiple login prompts for 1password

--- home/.install-password-manager.sh
+++ home/.install-password-manager.sh
@@ -3,21 +3,23 @@
 DO_INSTALL=$(grep -P '^[[:space:]]+install_op' ${HOME}/.config/chezmoi/chezmoi.toml | cut -d '=' -f2 | awk '{print $1}')
 [[ "${DO_INSTALL}" == "false" ]] && exit
 
-if ! [ -f "${HOME}/bin/op" ]; then
+readonly HOME_BIN="${HOME}/bin"
 
-    mkdir -p "${HOME}/bin"
+if ! [ -f "${HOME_BIN}/op" ]; then
+
+    mkdir -p "${HOME_BIN}"
     sudo apt-get install unzip
 
     wget "https://cache.agilebits.com/dist/1P/op2/pkg/v2.30.0/op_linux_amd64_v2.30.0.zip" -O op.zip
     unzip -d op op.zip
-    sudo mv op/op ${HOME}/bin/
+    sudo mv op/op ${HOME_BIN}/
     rm -r op.zip op
     sudo groupadd -f onepassword-cli
-    sudo chgrp onepassword-cli ${HOME}/bin/op
-    sudo chmod g+s ${HOME}/bin/op
+    sudo chgrp onepassword-cli ${HOME_BIN}/op
+    sudo chmod g+s ${HOME_BIN}/op
 
     # This file can't be a template, so we need to scrape the config file for the
     # email address.
     EMAIL=$(grep -P '^[[:space:]]+email' ${HOME}/.config/chezmoi/chezmoi.toml | cut -d '"' -f2)
-    ${HOME}/bin/op account add --address my.1password.com --email "${EMAIL}"
+    ${HOME_BIN}/op account add --address my.1password.com --email "${EMAIL}"
 fi
