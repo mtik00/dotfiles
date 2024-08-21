2b2362a0d33a97a055fbac0a3ecee39064b48ac2
Timothy McFadden <mtik00@users.noreply.github.com>
2024-08-21 08:59:48 -0600

    fix: typo in update

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ PATH="~/bin:${PATH}"
 
 # Set up files in the home directory
 if [[ -d ~/.local/share/chezmoi/.git ]]; then
-    chezmoi update  printf "\n%s\n" "...Chezmoi update complete"
+    chezmoi update && printf "\n%s\n" "...Chezmoi update complete"
 else
     pushd "${HOME}" && sh -c "$(curl -fsLS get.chezmoi.io)" -- init --apply mtik00 && popd && printf "\n%s\n" "...Chezmoi update complete"
 fi
