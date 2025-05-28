e4a65a7fe24d618f72c10c9f8fb174abb56671ba
Timothy McFadden <mtik00@users.noreply.github.com>
2025-05-28 09:46:47 -0600

    feat: adding uv install

---
+++ dotfiles/.installers/uv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -euo pipefail
+
+## Install starship
+if ! command -v uv > /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+else
+    echo "uv already installed"
+fi
