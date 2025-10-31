762f9bd775db1f53146378d0dfd5190629d403b9
Timothy McFadden <mtik00@users.noreply.github.com>
2025-10-31 14:09:59 -0600

    feat: update uv if already installed

--- dotfiles/.installers/uv.sh
+++ dotfiles/.installers/uv.sh
@@ -5,5 +5,6 @@ set -euo pipefail
 if ! command -v uv > /dev/null; then
     curl -LsSf https://astral.sh/uv/install.sh | sh
 else
-    echo "uv already installed"
+    echo "uv already installed; updating"
+    uv self update
 fi
