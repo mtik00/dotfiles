4fc7e37828646c49dce256ead61ee1d12a8f38ef
Timothy McFadden <mtik00@users.noreply.github.com>
2025-10-31 13:54:23 -0600

    refactor: removing poetry and pyenv

--- dotfiles/.installers/00-setup.sh
+++ dotfiles/.installers/00-setup.sh
@@ -2,8 +2,7 @@
 set -euo pipefail
 
 export HOME_BIN="${HOME}/bin"
-export PYENV_ROOT="${HOME}/.local/share/pyenv"
-export PATH="${HOME}/bin:${HOME}/.local/share/pyenv/bin:$PATH"
+export PATH="${HOME}/bin:$PATH"
 
 # Make sure this is here and its in the path
 mkdir -p "${HOME}/bin"
--- dotfiles/.installers/keeper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-
-# Install keeper commander CLI
-if ! command -v keeper > /dev/null; then
-    $(pyenv which python) -m pip install --upgrade keepercommander
-else
-    echo "keeper already installed"
-fi
