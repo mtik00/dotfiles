0c1524c8a0e73e944b7aeb5133de0d749fa56694
Timothy McFadden <mtik00@users.noreply.github.com>
2026-01-15 16:21:09 -0700

    feat: adding mise

--- dotfiles/.installers/mise.sh
+++ dotfiles/.installers/mise.sh
@@ -3,12 +3,7 @@ set -euo pipefail
 
 # Install kind
 if ! command -v mise > /dev/null; then
-    sudo apt update -y && sudo apt install -y curl
-    sudo install -dm 755 /etc/apt/keyrings
-    curl -fsSL https://mise.jdx.dev/gpg-key.pub | sudo gpg --dearmor -o /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
-    echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
-    sudo apt update -y
-    sudo apt install -y mise
+    curl https://mise.run | MISE_INSTALL_PATH=~/.local/bin/mise sh
 else
     echo "mise already installed"
 fi
