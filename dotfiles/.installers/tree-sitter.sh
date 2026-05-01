227db8d4345f7b200072198cdcd9e57614e8c47d
Timothy McFadden <mtik00@users.noreply.github.com>
2026-05-01 10:23:26 -0600

    fix: tree-sitter

--- dotfiles/.installers/tree-sitter.sh
+++ dotfiles/.installers/tree-sitter.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 ## Install treesitter
 if ! command -v tree-sitter > /dev/null; then
-    readonly TREE_SITTER_LATEST_URL=$(curl -s https://api.github.com/repos/tree-sitter/tree-sitter/releases/latest | grep browser_download_url | grep linux-x86.gz | cut -d '"' -f 4)
+    readonly TREE_SITTER_LATEST_URL=$(curl -s https://api.github.com/repos/tree-sitter/tree-sitter/releases/latest | grep browser_download_url | grep linux-x64.gz | cut -d '"' -f 4)
     curl -L -o /tmp/tree-sitter.gz "${TREE_SITTER_LATEST_URL}"
     gunzip /tmp/tree-sitter.gz -c > "$HOME/bin/tree-sitter"
     chmod +x "$HOME/bin/tree-sitter"
-    rm /tmp/tree-sitter.tgz
+    rm -f /tmp/tree-sitter.gz
 else
-    echo "tree-sitter already installed"
+    echo "tree-sitter already installed: $(command -v tree-sitter)"
 fi
